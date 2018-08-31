79679bb9b9cb3ba9947fd016218da2b175f1e23f
Dev Say <game@devsay.com>
2018-08-31 20:27:15 -0300

    nue on admin

--- routes/admin.js
+++ routes/admin.js
@@ -9,11 +9,16 @@ const loginGetController = require('../controllers/admin/login_get')
 const loginPostController = require('../controllers/admin/login_post')
 const indexGetController = require('../controllers/admin/admin')
 
+const  postNewBlogController = require('../controllers/blog/post_new_blog')
+const POST_newBlogController = require('../controllers/blog/POST')
+
 router.get('/register', registerGetController.register_get)
 router.get('/login', loginGetController.login_get)
 router.get('/', isAuth, indexGetController.get_admin)
+router.get('/new', isAuth, postNewBlogController.add_blog)
 
 router.post('/register', registerPostController.register_post)
 router.post('/login', loginPostController.login_post)
+router.post('/new', isAuth, POST_newBlogController.blog_post)
 
 module.exports = router
